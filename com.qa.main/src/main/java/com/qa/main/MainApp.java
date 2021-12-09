db718cdfad574836ec1e7b34241435e6d94ae5df
Ammar Antaain Ikhrom <95301010+ammar1495@users.noreply.github.com>
2021-12-09 09:39:08 +0700

    Add files via upload

---
+++ com.qa.main/src/main/java/com/qa/main/MainApp.java
@@ -0,0 +1,9 @@
+package com.qa.main;
+
+public class MainApp {
+public static void main(String[] args) {
+		
+	}
+	
+
+}
---
+++ com.qa.main/src/main/java/com/qa/main/function/function.java
@@ -0,0 +1,10 @@
+package com.qa.main.function;
+
+public class function {
+public double hitungKecepatan(double jarak, double waktu) {
+		
+		return jarak/waktu;
+	}
+
+
+}
